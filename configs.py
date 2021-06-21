03272ac0fbf8b2e77bebaf6f6bbfa163ba520b73
m.ahadi <mojtaba_2424@yahoo.com>
2021-06-21 13:51:25 +0430

    fixed logger without configuration

--- configs.py
+++ configs.py
@@ -4,6 +4,6 @@ PERIOD = int(os.getenv("SITE_VISIT_PERIOD", 1))
 URL = os.getenv("URL")
 USE_PROXY = os.getenv("USE_PROXY", "False").lower() == "true"
 
-LOG_LEVEL = os.getenv("LOG_LEVEL").upper()
+LOG_LEVEL = os.getenv("LOG_LEVEL",'info').upper()
 
 PARALLELS = int(os.getenv("PARALLELS", 1))
